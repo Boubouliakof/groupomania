21ce84247f6a8fb55acc012d6a9c92c79eb8efbe
Olivier Boucher <olive.boubou53@gmail.com>
2020-08-04 19:34:21 +0200

    petite modif

--- frontend/src/components/items/ItemCard.vue
+++ frontend/src/components/items/ItemCard.vue
@@ -7,10 +7,10 @@
                         <b-img :src="item.User.imgUrl" width="64" :alt="item.User.firstname" rounded="circle"></b-img>
                     </template>
 
-                    <h5 class="mt-0">{{ item.User.firstname }} {{ item.User.lastname }}</h5>
+                    <h4 class="mt-0">{{ item.User.firstname }} {{ item.User.lastname }}</h4>
                     <p>{{ item.User.service }}</p>
-                    <b-img :src="item.imageUrl" fluid alt="" style="max-width: 200px; float: left; margin-right: 20px;"></b-img>
-                    <p>{{ item.title }}</p>
+                    <b-img :src="item.imageUrl" fluid alt="" style="float: left; margin-right: 20px; margin-bottom: 10px;"></b-img>
+                    <h5>{{ item.title }}</h5>
                     <p>{{ item.description }}</p>
                     <p>Prix : {{ item.price }} €</p>
                 </b-media>
